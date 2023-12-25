7bd2483bf7c96ab2ebf1609c0238082bf4ef401b
khaled0915 <khaledsaifullah50956@gmail.com>
2023-12-25 14:15:00 +0600

    created a from using react hook form

--- src/Pages/Dashboard/AddTask/AddTask.jsx
+++ src/Pages/Dashboard/AddTask/AddTask.jsx
@@ -1,14 +1,127 @@
+import { useForm } from "react-hook-form";
 
 const AddTask = () => {
-    return (
-        <div className="mt-10">
 
-            <h3 className="text-black font-bold text-3xl"> here you can add your task </h3>
+
+
+
+    const {register , handleSubmit , reset} = useForm()
+
+    const onSubmit = (data) => {
+        
+        console.log(data)
+
+        reset();
+
+    }
+
+
+
+    
+    
+    
+    
+
+
+
+
+
+    return (
+        <div className="mt-10 mr-5 ml-5 bg-gray-200">
+
+            <h3 className=" font-bold text-center  text-3xl mb-10 underline text-sky-400"> here you can add your task </h3>
+
+
+            
+
+
+
+
+
+
+
+     <form onSubmit={handleSubmit(onSubmit)}   className="card-body">
+        <div className="form-control">
+
+
+          <label className="label">
+            <span className=" text-lg  fon-bold label-text"> Title </span>
+          </label>
+
+
+          <input {...register("title")}  placeholder="title" className="input input-bordered" required />
+
+
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="text-lg font-bold mx-auto label-text">Priority</span>
+          </label>
+
+
+
+          <select {...register("priority")} className="select mx-auto select-warning w-full max-w-xs text-center" >
+  <option disabled selected>select your priority</option>
+  <option value="low"> Low</option>
+  <option value="moderate"> Moderate</option>
+  <option value="high">High</option>
+  
+</select>
+
+
+
+<textarea {...register("desc")}   className="textarea  textarea-warning mt-8" placeholder="add a description"></textarea>
+
+
+
+<div className="mt-5">
+<label className="label">
+            <span className="label-text"> Deadline </span>
+          </label>
+
+
+          <input {...register("deadline")}  placeholder="deadline" className="input  input-bordered" required />
+</div>
+
+
+
+
+
+        
+         
+        </div>
+        <div className="form-control mt-6">
+          
+
+
+          <input className="btn btn-success" type="submit" />
+
+
+
+        </div>
+      </form>
+
+
+
+
+
+    
+
+
+
+
+
+
 
           
             
         </div>
+
+
+
+
     );
-};
+
+    }
 
 export default AddTask;
